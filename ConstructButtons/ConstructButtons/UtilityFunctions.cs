d56668f43908b5a3a1139544ac4927b830cff4c9
Peter N K <pnk@avb.bg>
2018-07-19 17:15:18 +0300

    Ready for use. I have to finish this. Created in version 2.0.40. This test is ready for Intersecion, Width and Circle only

---
+++ ConstructButtons/ConstructButtons/UtilityFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConstructButtons
+{
+    class ReportAction
+    {
+        public static void PositiveReport(string Actual)
+        {
+            string strActualText = "Sucessfully Performed: " + Actual;
+            Ranorex.Report.Success(strActualText);
+        }
+
+        public static void NegativeReport(string Actual, string Negative)
+        {
+            string strActualText = "The Actual Value is :" + Actual;
+            Ranorex.Report.Error(strActualText);
+
+            string strHardcodedText = "The Expected Text is :" + Negative;
+            Ranorex.Report.Error(strHardcodedText);
+        }
+    }
+
+    class StringCompare
+    {
+        public static void Text (string Actual, string Hardcoded)
+        {
+            string strReadText = Actual;
+            string strHardcoded = Hardcoded;
+
+
+            if (strReadText == strHardcoded)
+            {
+                ReportAction.PositiveReport(strReadText);
+            }
+            else
+            {
+                ReportAction.NegativeReport(strReadText, strHardcoded);
+            }
+        }
+    }
+}
